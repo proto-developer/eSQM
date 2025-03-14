f57a05b9a85443c0b6ba762a9d249b72081407c9
Babar78 <muhammadbabarashraf8@gmail.com>
2025-03-15 02:28:21 +0500

    fix: remove unnecessary dependency from useEffect in MondayContextProvider

--- workflow_item_view/src/utils/MondayContext.jsx
+++ workflow_item_view/src/utils/MondayContext.jsx
@@ -16,7 +16,7 @@ export const MondayContextProvider = ({ children }) => {
       setSessionToken(res.data);
     });
     monday.execute("valueCreatedForUser");
-  }, [monday]);
+  }, []);
 
   return (
     <MondayContext.Provider
